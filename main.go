bea4a2462b857fe275ae9d2969c8118a6882cc75
zombfeed <rdgammad@gmail.com>
2026-01-20 15:47:42 -0800

    CH1:L8/L12 serve an image / add handler

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	smux := http.NewServeMux()
-	server := http.Server{}
-	server.Handler = smux
-	server.Addr = ":8080"
+	const filepathRoot = "."
+	const port = "8080"
 
-	server.ListenAndServe()
+	smux := http.NewServeMux()
+	smux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	smux.HandleFunc("/healthz", handlerReadiness)
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: smux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
