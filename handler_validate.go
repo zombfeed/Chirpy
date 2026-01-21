424941e2efa87db1e1de6f8f8b0cd7fc4d9a333f
zombfeed <rdgammad@gmail.com>
2026-01-21 12:45:11 -0800

    CH4:L6 add profanity filter

--- handler_validate.go
+++ handler_validate.go
@@ -3,14 +3,18 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 )
+
+var profanity = []string{"kerfuffle", "sharbert", "fornax"}
 
 func handlerValidate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -25,5 +29,16 @@ func handlerValidate(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
-	respondWithJSON(w, 200, returnVals{Valid: true})
+	respondWithJSON(w, 200, returnVals{CleanedBody: profanityFilter(params.Body)})
+}
+
+func profanityFilter(body string) string {
+	splitBody := strings.Split(body, " ")
+	for i, word := range splitBody {
+		lword := strings.ToLower(word)
+		if slices.Contains(profanity, lword) {
+			splitBody[i] = "****"
+		}
+	}
+	return strings.Join(splitBody, " ")
 }
